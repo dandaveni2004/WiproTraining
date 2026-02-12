d438c210892ca67c680944831734eb736af21524
dandaveni2004 <ganeshdandaveni@gmail.com>
2026-02-12 22:11:52 +0530

    first commit

---
+++ Assignment/Assignment/PalindromeCount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class PalindromeCount
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a Sentance");
+            string input = Console.ReadLine();
+            input = input.ToLower();
+            string[] words = input.Split(' ');
+            int count = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                string word = words[i];
+                string reversed = "";
+                for (int j = word.Length - 1; j >= 0; j--)
+                {
+                    reversed += word[j];
+                }
+                if (word == reversed)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Count of Palindrome words: " + count);
+        }
+    }
+}
---
+++ Assignment/Assignment/ReverseAltWord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class ReverseAltWord
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Sentance");
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    string reversed = "";
+                    for (int j = words[i].Length - 1; j >= 0; j--)
+                    {
+                        reversed = reversed + words[i][j];
+                    }
+                    words[i] = reversed;
+                }
+            }
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i] + " ");
+            }
+        }
+    }
+}
